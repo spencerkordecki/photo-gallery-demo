f817889a9c13890ab7b026da78ea55afe9b6e6cf
Spencer <spencerkordecki@gmail.com>
2020-12-06 12:53:03 -0500

    add background color to app, add test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the react carousel demo header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/react carousel demo/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders the first image in the carousel', () => {
+  render(<App />);
+  const firstImageCaption = screen.getByText(/"Bespoke Manufacturing"/i);
+  expect(firstImageCaption).toBeInTheDocument();
+});
+
+test('renders the second image in the carousel', () => {
+  render(<App />);
+  const secondImageCaption = screen.getByText(/"Down the Path"/i);
+  expect(secondImageCaption).toBeInTheDocument();
+});
+
+test('renders the third image in the carousel', () => {
+  render(<App />);
+  const thirdImageCaption = screen.getByText(/"Clearing the Mind"/i);
+  expect(thirdImageCaption).toBeInTheDocument();
+});
+
+test('renders all images with alt text', () => {
+  render(<App />);
+  const imageAltText = screen.getAllByAltText(/\D/);
+  expect(imageAltText).toHaveLength(3);
 });
